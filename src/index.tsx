485cbcfb390aa93bb618679f04bea4a3daad5c79
beom220 <beom220@gmail.com>
2022-07-30 22:52:13 +0900

    warning code fix

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { worker } from "@/mock/worker";
 
-worker.start();
+(async () => {
+    const {worker} = await import('@/mock/worker');
+    return worker.start();
+})();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -15,7 +18,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
