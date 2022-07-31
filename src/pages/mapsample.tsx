aa6a280bbce275b709332826b0514060c6032ef1
beom220 <beom220@gmail.com>
2022-08-01 00:33:19 +0900

    init env type set

--- src/pages/mapsample.tsx
+++ src/pages/mapsample.tsx
@@ -1,27 +1,37 @@
-import {Map, MapMarker} from "react-kakao-maps-sdk";
-import {Grid, Segment} from "semantic-ui-react";
+import {Map, MapMarker, useInjectKakaoMapApi} from "react-kakao-maps-sdk";
+import {Grid, Placeholder, Segment} from "semantic-ui-react";
 import Helper from "@/pages/helper";
 
 export default function MapSample() {
-    const whereAt = {
-        lat: 37.47943754699013, lng: 126.82406106502174
+    const {loading, error} = useInjectKakaoMapApi({
+        appkey: process.env.REACT_APP_KAKAOMAP_API_KEY.toString()
+    })
+
+    const coordinate = {
+        lat: 37.53702976188573, lng: 127.00923267432718
     }
     return (
         <Grid style={{marginTop: "20px"}}>
             <Grid.Row centered columns={2}>
                 <Grid.Column centered="true">
                     <Segment>
-                        <Map
-                            center={whereAt}
-                            level={3}
-                            draggable={true}
-                            zoomable={true}
-                            style={{width: "100%", height: "200px"}}
-                        >
-                            <MapMarker position={whereAt}>
-                                <div style={{color: "#000"}}>집</div>
-                            </MapMarker>
-                        </Map>
+                        {loading ?
+                            <Placeholder style={{width: "100%", height: 200, maxWidth: "unset"}}>
+                                <Placeholder.Image/>
+                            </Placeholder> :
+                            <Map
+                                center={coordinate}
+                                level={3}
+                                draggable={true}
+                                zoomable={true}
+                                style={{width: "100%", height: "200px"}}
+                            >
+                                <MapMarker position={coordinate}>
+                                    <div style={{color: "#000", textAlign: "center"}}>집</div>
+                                </MapMarker>
+                            </Map>
+                        }
+
                     </Segment>
                     <Helper/>
                 </Grid.Column>
