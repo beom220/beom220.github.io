67c554aa20b04d2af76d4013d44115e4534d3d55
beom220 <beom220@gmail.com>
2022-08-08 00:35:42 +0900

    refactor

--- src/pages/product/product.tsx
+++ src/pages/product/product.tsx
@@ -1,12 +1,33 @@
 import Template from "@/components/template";
-import {default as List} from "@/components/products/list";
 import {Container} from "semantic-ui-react";
+import List from "@/components/products/list"
+import Detail from "@/components/products/detail";
+import Create from "@/components/products/create";
 
-export default function ProductList() {
+export function ProductList() {
     return (
         <Template>
             <Container>
                 <List/>
+            </Container>
+        </Template>
+    )
+}
+
+export function ProductDetail(){
+    return (
+        <Template>
+            <Container>
+                <Detail/>
+            </Container>
+        </Template>
+    )
+}
+export function ProductCreate(){
+    return (
+        <Template>
+            <Container>
+                <Create/>
             </Container>
         </Template>
     )
