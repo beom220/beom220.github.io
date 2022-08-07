e939c021c29e22f40dcca07e84e12e5faa0dd349
beom220 <beom220@gmail.com>
2022-08-07 20:12:02 +0900

    modify : layout

--- src/components/join/loginForm.tsx
+++ src/components/join/loginForm.tsx
@@ -1,34 +1,28 @@
-import {Button, Form} from "semantic-ui-react";
-import {useForm, SubmitHandler} from "react-hook-form";
 import {ChangeEvent, useEffect} from "react";
+import {Button, Form, Grid} from "semantic-ui-react";
+import {useForm, SubmitHandler} from "react-hook-form";
+import {MemberType} from "@/types/member";
 import {useLogin} from "@/api";
 import useSession from "@/hooks/useSession";
-import {MemberType} from "@/types/member";
 import useModals from "@/hooks/useModals";
-import styled from "@emotion/styled";
 import {AlertPortal} from "@/components/common";
 import {useRecoilState} from "recoil";
 import {memberState} from "@/app/member";
 import {useNavigate} from "react-router";
 
 export default function LoginForm() {
+    const [member, setMember] = useRecoilState(memberState);
     const {register, handleSubmit, setValue, trigger, formState: {errors}} = useForm();
     const {isOpen, handleModal, message, handleMessage} = useModals();
     const {GetSession, SetSession} = useSession();
-    const [member, setMember] = useRecoilState(memberState);
     const navigate = useNavigate();
 
     /* 로그인 유저라면 페이지 이동*/
     useEffect(() => {
-        if(member) navigate('/');
-    },[member])
+        if (member) navigate('/');
+    }, [member])
 
-    const onChange = async (e: ChangeEvent<HTMLInputElement>) => {
-        const {name, value} = e.target;
-        setValue(name, value);
-        await trigger(name.toString());
-    }
-
+    /* input validation */
     useEffect(() => {
         register("email", {
             required: true,
@@ -39,6 +33,12 @@ export default function LoginForm() {
             minLength: 4
         });
     }, []);
+
+    const onChange = async (e: ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setValue(name, value);
+        await trigger(name.toString());
+    }
 
     const checkErrorPoint = (name: string, isError: boolean) => {
         if (isError) {
@@ -59,56 +59,65 @@ export default function LoginForm() {
             * Response 있는 에러 && 500 미만인 에러들은 화면에서 처리
             * 그 이상의 에러는 에러화면으로 이동
             * */
-            onError: (error:any) => {
-                if(error.response){
+            onError: (error: any) => {
+                if (error.response) {
                     const {status, data} = error.response;
-                    if(status >= 500) return navigate('/error')
+                    if (status >= 500) return navigate('/error')
                     handleMessage({
                         content: (data)
                     })
-                    return handleModal(true);
+                    return handleModal(true)
                 }
                 return navigate('/error')
             },
             onSuccess: (data) => {
                 const {data: message} = data;
                 SetSession('user', message);
-                setMember(GetSession('user'))
+                setMember(GetSession('user'));
             }
         })
     }
 
     return (
         <Form onSubmit={handleSubmit(onSubmit)}>
-            <Input
-                name="email"
-                fluid
-                icon='user'
-                iconPosition='left'
-                placeholder="email"
-                disabled={isLoading}
-                onChange={onChange}
-                error={checkErrorPoint("email", !!errors.email)}
-            />
-            <Input
-                name="password"
-                fluid
-                icon='lock'
-                iconPosition='left'
-                type="password"
-                placeholder="password"
-                disabled={isLoading}
-                onChange={onChange}
-                error={checkErrorPoint("password", !!errors.password)}
-            />
-            <Button loading={isLoading} disabled={isLoading} color="violet" fluid type="submit">로그인</Button>
+            <Grid>
+                <Grid.Row>
+                    <Grid.Column
+                        as={Form.Input}
+                        name="email"
+                        fluid
+                        icon='user'
+                        iconPosition='left'
+                        placeholder="email"
+                        disabled={isLoading}
+                        onChange={onChange}
+                        error={checkErrorPoint("email", !!errors.email)}
+                        style={{fontSize: '16px'}}
+                    />
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column
+                        as={Form.Input}
+                        name="password"
+                        fluid
+                        icon='lock'
+                        iconPosition='left'
+                        type="password"
+                        placeholder="password"
+                        disabled={isLoading}
+                        onChange={onChange}
+                        error={checkErrorPoint("password", !!errors.password)}
+                        style={{fontSize: '16px'}}
+                    />
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column>
+                        <Button loading={isLoading} disabled={isLoading} color="violet" fluid type="submit">로그인</Button>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
             <AlertPortal message={message} isOpen={isOpen} handler={() => handleModal(false)}/>
         </Form>
     )
 }
 
-
-const Input = styled(Form.Input)`
-  margin-bottom: 2rem !important;
-  text-align: left !important;
-`
