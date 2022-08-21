a6660f9aa303cc6c6d4baf325b09bce4cb7a79ce
beom220 <beom220@gmail.com>
2022-08-21 16:53:13 +0900

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect} from 'react';
-import 'semantic-ui-css/semantic.min.css';
 // import './app.css';
 import {BrowserRouter} from "react-router-dom";
 import Router from "@/routes";
