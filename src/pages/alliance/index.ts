1968f712f6910cddebdc7c2d8fa2c2fcd5bc661b
beom220 <beom220@gmail.com>
2022-09-11 13:47:20 +0900

    modify alliance

---
+++ src/pages/alliance/index.ts
@@ -0,0 +1,6 @@
+export {default as AllianceCreate} from "@/pages/alliance/create";
+export {default as AllianceList} from "@/pages/alliance/list";
+export {default as AllianceInfo} from "@/pages/alliance/info";
+export {default as AllianceService} from "@/pages/alliance/service";
+export {default as AllianceEditService} from "@/pages/alliance/editService";
+export {default as AllianceOption} from "@/pages/alliance/option";
